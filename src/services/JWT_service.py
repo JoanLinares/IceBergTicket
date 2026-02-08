df33531d85cbe7c321423137ab9d033ee42ace90
JoanLinares <joan.linares@gracia.lasalle.cat>
2026-02-08 16:40:40 +0100

    feat: Json web tokens service for register and login

---
+++ src/services/JWT_service.py
@@ -0,0 +1,13 @@
+# src/services/jwt_service.py
+import jwt
+import datetime
+import os
+
+SECRET = os.getenv("JWT_SECRET", "dev_secret")
+
+def create_token(user_id):
+    payload = {
+        "user_id": user_id,
+        "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=24)
+    }
+    return jwt.encode(payload, SECRET, algorithm="HS256")
