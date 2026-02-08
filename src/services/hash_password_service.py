a743790f1f0eb31bbe0fc91b2677e0996321dc20
JoanLinares <joan.linares@gracia.lasalle.cat>
2026-02-08 16:40:51 +0100

    feat: hash password service

---
+++ src/services/hash_password_service.py
@@ -0,0 +1,21 @@
+# src/services/password_service.py
+import re
+from werkzeug.security import generate_password_hash
+
+def validate_password(password):
+    if len(password) < 8:
+        return False, "Password must be at least 8 characters"
+
+    if not re.search(r"[A-Z]", password):
+        return False, "Password must contain an uppercase letter"
+
+    if not re.search(r"[0-9]", password):
+        return False, "Password must contain a number"
+
+    if not re.search(r"[!@#$%^&*()_+=\-]", password):
+        return False, "Password must contain a special character"
+
+    return True, None
+
+def hash_password(password):
+    return generate_password_hash(password)
